e0c13287675b65b8436c728af2e7d51a8708dc13
imnotkind <imnotkind307@gmail.com>
2021-12-10 16:02:31 +0900

    install from vessl

---
+++ install_from_vessl.sh
@@ -0,0 +1,9 @@
+pip install gym pfrl
+pip install tensorflow-gpu==1.15.5
+pip install torch==1.8.1+cu102 -f  https://download.pytorch.org/whl/torch_stable.html
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
+sudo add-apt-repository --assume-yes ppa:sumo/stable
+sudo apt-get update
+sudo apt-get --assume-yes install sumo sumo-tools sumo-doc
+export SUMO_HOME=/usr/share/sumo
+cd ~/RESCO/environments/grid4x4/ && unzip grid4x4.zip
